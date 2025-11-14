278ec72630e61e75aa87abd950726c75e5266bfd
MartaPianta <marta.visetti@unifr.ch>
2025-11-14 17:40:02 +0100

    interface

---
+++ fuzzy project working page/app.ts
@@ -0,0 +1,25 @@
+const tempSlider = document.getElementById("Temp_slider") as HTMLInputElement;
+const humSlider = document.getElementById("Hum_slider") as HTMLInputElement;
+const tempValue = document.getElementById("temp_value")!;
+const humValue = document.getElementById("hum_value")!;
+
+async function updateFromSensor() {
+  try {
+    const res = await fetch("http://localhost:5000/data");
+    const data = await res.json();
+
+    // Aggiorna valori numerici
+    tempValue.textContent = data.temperature.toFixed(1);
+    humValue.textContent = data.humidity.toFixed(1);
+    console.log(tempValue.textContent)
+
+    // Aggiorna slider coerenti con i limiti impostati
+    tempSlider.value = data.temperature.toString();
+    humSlider.value = data.humidity.toString();
+  } catch (err) {
+    console.error("Errore durante la lettura del sensore:", err);
+  }
+}
+
+// aggiorna ogni 2 secondi
+setInterval(updateFromSensor, 2000);
